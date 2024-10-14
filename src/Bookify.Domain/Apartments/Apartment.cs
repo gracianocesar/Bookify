b4bc724d3f0cfd932ab60ae7506f3e7153b7d845
gracianocesar <gracianocesar@gmail.com>
2024-10-14 06:56:39 -0300

    Refactoring Id Entity

--- src/Bookify.Domain/Apartments/Apartment.cs
+++ src/Bookify.Domain/Apartments/Apartment.cs
@@ -1,8 +1,14 @@
-﻿namespace Bookify.Domain.Apartments
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Apartments
 {
-    public sealed class Apartment
+    public sealed class Apartment : Entity
     {
-        public Guid Id { get; private set; }
+        public Apartment(Guid id) 
+            : base(id)
+        {
+        }
+
         public string Name { get; private set; }
         public string Description { get; private set; }
 
