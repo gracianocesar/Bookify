1272cbfa60b581604c269552e4378704bddc5e6a
gracianocesar <gracianocesar@gmail.com>
2024-10-20 00:01:50 -0300

    Add new version

--- src/Bookify.Domain/Apartments/Apartment.cs
+++ src/Bookify.Domain/Apartments/Apartment.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Shared;
 
 namespace Bookify.Domain.Apartments
 {
@@ -24,7 +25,7 @@ namespace Bookify.Domain.Apartments
 
         public Money CleaningFee { get; private set; }
 
-        public DateTime? LastBookedOnUtc { get; private set; }
+        public DateTime? LastBookedOnUtc { get; internal set; }
 
         public List<Amenity> Amenities { get; private set; } = new();
     }
---
+++ src/Bookify.Domain/Shared/Money.cs
@@ -0,0 +1,20 @@
+﻿namespace Bookify.Domain.Shared
+{
+    public record Money(decimal Amount, Currency Currency)
+    {
+        public static Money operator +(Money firts, Money second)
+        {
+            if (firts.Currency != second.Currency)
+            {
+                throw new InvalidOperationException("Currencies have to be equal");
+            }
+
+            return new Money(firts.Amount + second.Amount, firts.Currency);
+        }
+
+        public static Money Zero() => new(0, Currency.None);
+
+        public static Money Zero(Currency currency) => new(0,currency);
+        public bool IsZero() => this == Zero(Currency);
+    }
+}
